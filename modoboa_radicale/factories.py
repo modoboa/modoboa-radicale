8f903123d942dd47ad4eacd8e7a0d235c71d4bdf
Antoine Nguyen <tonio@ngyn.org>
2015-02-11 13:11:57 +0100

    Initial commit.

---
+++ modoboa_radicale/factories.py
@@ -0,0 +1,37 @@
+"""
+Fixtures factories.
+"""
+import factory
+
+from modoboa_admin.factories import (
+    DomainFactory, MailboxFactory
+)
+
+from . import models
+
+
+class UserCalendarFactory(factory.DjangoModelFactory):
+
+    class Meta:
+        model = models.UserCalendar
+
+    name = factory.Sequence(lambda n: 'User calendar %s' % n)
+    mailbox = factory.SubFactory(MailboxFactory)
+
+
+class SharedCalendarFactory(factory.DjangoModelFactory):
+
+    class Meta:
+        model = models.SharedCalendar
+
+    name = factory.Sequence(lambda n: 'Shared calendar %s' % n)
+    domain = factory.SubFactory(DomainFactory)
+
+
+class AccessRuleFactory(factory.DjangoModelFactory):
+
+    class Meta:
+        model = models.AccessRule
+
+    mailbox = factory.SubFactory(MailboxFactory)
+    calendar = factory.SubFactory(UserCalendarFactory)
---
+++ modoboa_radicale/modo_extension.py
@@ -0,0 +1,32 @@
+"""
+Radicale management frontend.
+
+"""
+
+from django.utils.translation import ugettext_lazy
+
+from modoboa.core.extensions import ModoExtension, exts_pool
+from modoboa.lib import parameters
+
+
+class Radicale(ModoExtension):
+
+    """Radicale extension declaration."""
+
+    name = "modoboa_radicale"
+    label = ugettext_lazy("Radicale management")
+    version = "1.0"
+    url = "calendars"
+    description = ugettext_lazy(
+        "Management frontend for Radicale, a simple calendar and contact "
+        "server."
+    )
+
+    def load(self):
+        """Plugin loading."""
+        from .app_settings import ParametersForm
+
+        parameters.register(ParametersForm, "Radicale")
+        from . import general_callbacks
+
+exts_pool.register_extension(Radicale)
