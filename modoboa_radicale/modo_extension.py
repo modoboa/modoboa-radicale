36a16c6a1477253a1255c1bd3dca63ec7944fcfb
Antoine Nguyen <tonio@ngyn.org>
2015-04-30 15:24:35 +0200

    Version bumped to 1.0.1

--- modoboa_radicale/modo_extension.py
+++ modoboa_radicale/modo_extension.py
@@ -15,7 +15,7 @@ class Radicale(ModoExtension):
 
     name = "modoboa_radicale"
     label = ugettext_lazy("Radicale management")
-    version = "1.0.0"
+    version = "1.0.1"
     url = "calendars"
     description = ugettext_lazy(
         "Management frontend for Radicale, a simple calendar and contact "
