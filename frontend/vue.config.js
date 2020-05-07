24ca3470f96923f29bac66fdd629439a864bf37e
Antoine Nguyen <tonio@ngyn.org>
2020-05-07 16:09:44 +0200

    Fixed wrong static path for production build.

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -3,7 +3,7 @@ var BundleTracker = require('webpack-bundle-tracker');
 
 module.exports = {
     publicPath: process.env.NODE_ENV === 'production'
-              ? '/static/'
+              ? '/sitestatic/'
               : 'http://localhost:8080/',
     outputDir: '../modoboa_radicale/static/',
     assetsDir: 'modoboa_radicale',
