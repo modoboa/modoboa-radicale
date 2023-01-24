e1bb0c17d502543a4f52d240baeb31498991cdf7
Antoine Nguyen <tonio@ngyn.org>
2023-01-24 15:45:14 +0100

    Updated webpack loader config

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -2,24 +2,30 @@ var path = require('path')
 var BundleTracker = require('webpack-bundle-tracker');
 
 module.exports = {
-    publicPath: process.env.NODE_ENV === 'production'
-              ? '/sitestatic/'
-              : 'http://localhost:8080/',
-    outputDir: '../modoboa_radicale/static/',
-    assetsDir: 'modoboa_radicale',
-    devServer: {
-        publicPath: 'http://localhost:8080/',
-        headers: {
-            'Access-Control-Allow-Origin': '*',
-            'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, PATCH, OPTIONS',
-            'Access-Control-Allow-Headers':
-            'X-Requested-With, content-type, Authorization',
-            'Access-Control-Allow-Credentials': 'true'
-        }
-    },
-    configureWebpack: config => {
-        config.plugins.push(new BundleTracker({
-            path: '../modoboa_radicale/static/modoboa_radicale/'
-        }));
-    }
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/sitestatic/'
+    : 'http://localhost:8080/',
+  outputDir: '../modoboa_radicale/static/',
+  assetsDir: 'modoboa_radicale',
+  chainWebpack: config => {
+    config
+      .plugin('BundleTracker')
+      .use(BundleTracker, [{
+        filename: path.join('../modoboa_radicale/static/modoboa_radicale/', 'webpack-stats.json')
+      }])
+
+    config.devServer
+      .public('http://localhost:8080')
+      .port(8080)
+      .hotOnly(true)
+      .watchOptions({poll: 1000})
+      .https(false)
+      .headers({
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, PATCH, OPTIONS',
+        'Access-Control-Allow-Headers':
+        'X-Requested-With, content-type, Authorization',
+        'Access-Control-Allow-Credentials': 'true'
+      })
+  }
 }
